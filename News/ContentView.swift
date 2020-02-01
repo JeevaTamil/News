15f7fb58748de1942a4dd44e82c92601703f8fb4
Azhagusundaram.t <Azhagusundaram.t@zohocorp.com>
2020-02-01 12:54:36 +0530

    Initial Commit

---
+++ News/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  News
+//
+//  Created by Azhagusundaram Tamil on 01/02/20.
+//  Copyright © 2020 Azhagusundaram Tamil. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
